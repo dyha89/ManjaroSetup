d2bfdc1bdea42faaa38b367e2208fdddc9fd7352
dyha89 <dyha89@gmail.com>
2021-12-15 23:14:56 +0100

    ssh added

--- setup.sh
+++ setup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-APPS="vim lutris w3m wine"
+APPS="vim lutris w3m wine openssh"
 
 pacman -Syu
 
 pacman -S $APPS --noconfirm
+
+systemctl enable sshd.service
+systemctl start sshd.service
 
 # https://tuxfixer.com/configure-ssh-service-in-manjaro-linux/
