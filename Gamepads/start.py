7e9f6b7493841a3345e54365065f2075df6982e6
dyha89 <dyha89@gmail.com>
2021-12-19 12:36:47 +0100

    working pads

--- Gamepads/start.py
+++ Gamepads/start.py
@@ -1,5 +1,6 @@
 import re
 import os
+import subprocess
 
 current_dir = os.path.dirname(__file__)
 config_dir = os.path.join(current_dir, "Conf")
@@ -7,6 +8,8 @@ extension = ".xboxconf"
 event_path = "/dev/input/event"
 name_pattern = 'N: Name="(.+?)"'
 handlers_event_pattern = 'H: Handlers=(.*?)event(.[0-9]?)'
+
+processes = []
 
 with open('/proc/bus/input/devices','r') as file:
 
@@ -23,10 +26,19 @@ with open('/proc/bus/input/devices','r') as file:
         event = re.search(handlers_event_pattern, x).group(2)
         event_file = event_path + event
 
-        print(event_file)
-
-        #if(not os.path.isfile(event_file)):
-        #    continue
-
         args = ["xboxdrv","--evdev=" + event_file,"--config",config]
         print(args)
+
+        proc = subprocess.Popen(args)
+        processes.append(proc)
+
+input("Processes started.\n")
+
+print("Killing processes.")
+
+for p in processes:
+    p.kill()
+    print(p.args)
+
+print("Done")
+
