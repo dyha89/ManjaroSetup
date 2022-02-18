145c8742f991e803ef5f693ba5f19d9c0634a03a
dyha89 <dyha89@gmail.com>
2022-02-18 22:38:31 +0100

    gerbera

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-APPS="vim lutris w3m wine openssh yay tmux base-devel evtest lshw moc gnome-disk-utility ffmpeg ranger"
+APPS="vim lutris w3m wine openssh yay tmux base-devel evtest lshw moc gnome-disk-utility ffmpeg ranger gerbera"
 
 sudo pacman -Syu --noconfirm
 
@@ -9,8 +9,13 @@ sudo pacman -S $APPS --noconfirm
 sudo systemctl enable sshd.service
 sudo systemctl start sshd.service
 
-YAYAPPS="xboxdrv foobar2000 enca"
-#dotnet-sdk-bin
+sudo useradd --system gerbera
+sudo mkdir /etc/gerbera
+sudo chown -Rv gerbera:gerbera /etc/gerbera
+sudo systemctl daemon-reload
+sudo systemctl start gerbera
+
+YAYAPPS="xboxdrv foobar2000 enca dotnet-sdk-bin"
 
 yay -S $YAYAPPS --noconfirm
 
--- tmux_ide_panel.sh
+++ tmux_ide_panel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Split panes horizontally and don't switch selection to the new pane to continue the script.
-tmux split-window -hd -l 50% vim
+tmux split-window -hd -l 50% "vim && bash"
 
 # vertical split with file manager
-tmux split-window -vdb -p 50 ranger
+tmux split-window -vdb -p 50 "ranger && bash"
